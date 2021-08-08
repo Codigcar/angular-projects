d89d3e990514df714d5d971a1d0de449e4b017de
Carlos Castilla <34387852+Codigcar@users.noreply.github.com>
2021-08-08 05:13:58 -0500

    bases - Funciones y variables

--- bases/src/app/app.component.ts
+++ bases/src/app/app.component.ts
@@ -3,9 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   public title: string = 'Contador App';
-  public numero: number = 10; 
+  public numero: number = 10;
+
+  sumar = () => {
+    this.numero += 1;
+  };
+
+  restar = () => {
+    this.numero -= 1;
+  }
+
+  acumular = (valor: number) => {
+    this.numero += valor;
+  }
+
+
 }
