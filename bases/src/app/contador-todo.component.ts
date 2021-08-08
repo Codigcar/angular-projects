d36492cae6e283dc2a88cd9e7acfd528721eb097
Carlos Castilla <34387852+Codigcar@users.noreply.github.com>
2021-08-08 05:57:38 -0500

    bases - Crear un component + configurarlo  y agregarlo en modulos

---
+++ bases/src/app/contador-todo.component.ts
@@ -0,0 +1,13 @@
+
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-contador',
+    template: `
+                <h1>App-Contador-Component</h1>
+    `
+})
+
+export class ContadorComponent {
+
+}
