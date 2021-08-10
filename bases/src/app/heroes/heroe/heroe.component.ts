c977e76ec12cdf5afb1d8b33d03acfe952c48596
Carlos Castilla <34387852+Codigcar@users.noreply.github.com>
2021-08-10 02:09:34 -0500

    bases - def variables, funciones y getFunciones

--- bases/src/app/heroes/heroe/heroe.component.ts
+++ bases/src/app/heroes/heroe/heroe.component.ts
@@ -5,5 +5,19 @@ import { Component } from "@angular/core";
     templateUrl: 'heroe.comoponent.html'
 })
 export class HeroeComponent{ 
+    nombre: string = 'Batman';
+    edad: number = 55;
 
+    obtenerNombre(): string {
+        return `${this.nombre} - ${this.edad}`;
+    }
+
+    NombreCapitalizado(nombre:string):string {
+        return nombre.toUpperCase();
+    }
+    
+    // get y set
+    get getNombreCapitalizado() {
+        return this.nombre.toUpperCase();
+    }
 }
