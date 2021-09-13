2994340241346a274cf5a53fc02191a079d52429
Carlos Castilla <34387852+Codigcar@users.noreply.github.com>
2021-09-12 19:58:08 -0500

    01 - Added CSS Sidebar y Section Buscar

--- 01-gifsApp/src/app/app.module.ts
+++ 01-gifsApp/src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatButtonModule,MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
