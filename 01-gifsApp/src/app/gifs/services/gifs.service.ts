935fde73ec4e024b473b6d29b34b9215d7ecd275
Carlos Castilla <34387852+Codigcar@users.noreply.github.com>
2021-09-13 11:28:01 -0500

    01 - LocalStorage para guardar el historial de busquedas y resultados

--- 01-gifsApp/src/app/gifs/services/gifs.service.ts
+++ 01-gifsApp/src/app/gifs/services/gifs.service.ts
@@ -7,15 +7,18 @@ import { Gif, SearchGifsResponse } from '../inteface/gifs.interface';
 })
 export class GifsService {
 
-  constructor(private http: HttpClient) { }
-
+  
   private apiKey: string = 'vfV8SOvefLtVRj5pOOTcRZgepQ0zs29o';
   private _historial: string[] = [];
-
+  
   //TODO: Cambiar any por su tipo
   private resultados: Gif[] = [];
-
-
+  
+  constructor(private http: HttpClient) {
+    this._historial = JSON.parse(localStorage.getItem('historial')!) || [];
+    this.resultados = JSON.parse(localStorage.getItem('resultados')!) || [];
+   }
+  
   get historial() {
     // romper la referencia con el operator spret, enviando una copia y no afectando al princiapl
     return [...this._historial];
@@ -35,15 +38,19 @@ export class GifsService {
 
       // Devolver solo los 10 primeros
       this._historial = this._historial.splice(0, 10);
-
       console.log('array: ', this._historial);
 
-      this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=${this.apiKey}&q=${query}&limit=10`)
-        .subscribe((resp) => {
-          console.log('resp http: ', resp.data)
-          this.resultados = resp.data;
-        })
+      // save busqueda en localStorage
+      localStorage.setItem('historial', JSON.stringify(this._historial));
     }
+
+    this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=${this.apiKey}&q=${query}&limit=10`)
+      .subscribe((resp) => {
+        console.log('resp http: ', resp.data)
+        this.resultados = resp.data;
+        // LS tiene limite de 50MB
+        localStorage.setItem('resultados',JSON.stringify(this.resultados)); 
+      })
 
   }
   public getResultados() {
