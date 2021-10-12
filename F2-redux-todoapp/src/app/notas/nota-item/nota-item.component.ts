73b257fc82d1c8c7c59ab51f05e13ffcd28c79f8
Carlos Castilla <34387852+Codigcar@users.noreply.github.com>
2021-10-11 21:16:30 -0500

    F2 - Mostrar el Notta-Item de todo un listado, hacer focus con doble click y el BLUR( quitar focus del campo cuando das click fuera)

--- F2-redux-todoapp/src/app/notas/nota-item/nota-item.component.ts
+++ F2-redux-todoapp/src/app/notas/nota-item/nota-item.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { NotaClass } from '../models/nota.model';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-nota-item',
@@ -7,9 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotaItemComponent implements OnInit {
 
+  @Input()
+  public notaChild!: NotaClass;
+
+  @ViewChild('inputFisico') txtInputFisico!: ElementRef;
+
+  public checkCompletado!: FormControl;
+  public txtInput!: FormControl;
+  public editando: boolean = false;
+
+
   constructor() { }
 
   ngOnInit(): void {
+    this.checkCompletado = new FormControl(this.notaChild.completado);
+    this.txtInput = new FormControl(this.notaChild.texto, Validators.required);
+  }
+
+  public editar() {
+    this.editando = true;
+    setTimeout(() => {
+      this.txtInputFisico.nativeElement.select();
+    }, 1);
+  }
+
+  public terminarEdicion(){
+    this.editando=false;
   }
 
 }
