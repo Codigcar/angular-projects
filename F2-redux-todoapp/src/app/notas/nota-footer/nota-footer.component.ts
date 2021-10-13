56d969a10945b003d0182ebb4396f845d5817613
Carlos Castilla <34387852+Codigcar@users.noreply.github.com>
2021-10-12 18:30:52 -0700

    F2 - Cambiar la variable Filtro con redux

--- F2-redux-todoapp/src/app/notas/nota-footer/nota-footer.component.ts
+++ F2-redux-todoapp/src/app/notas/nota-footer/nota-footer.component.ts
@@ -7,15 +7,24 @@ import { filtrosValidos } from '../redux/filtro.actions';
 @Component({
   selector: 'app-nota-footer',
   templateUrl: './nota-footer.component.html',
-  styleUrls: ['./nota-footer.component.scss']
+  styleUrls: ['./nota-footer.component.scss'],
 })
 export class NotaFooterComponent implements OnInit {
-
   filtroActual: actions.filtrosValidos = 'todos';
+  filtros: actions.filtrosValidos[] = [ 'todos','completados', 'pendientes'];
 
-  constructor(private store: Store<AppStateI>) { }
+  constructor(private store: Store<AppStateI>) {}
 
   ngOnInit(): void {
+    this.store
+      .select('filtro')
+      .subscribe((filtro) => (this.filtroActual = filtro));
   }
 
+  public cambiarFiltro(filtro: actions.filtrosValidos){
+    console.log('filtroActual: ',filtro);
+    this.store.dispatch(actions.setFiltroAction({filtro:filtro}))
+    
+    
+  }
 }
